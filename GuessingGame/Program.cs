0d73cb881bba7e57be4bf73355b90f295ed4ff76
Dakota <Dakotawhittle1203@gmail.com>
2020-05-14 12:13:51 -0400

    guessing-game-first-iteration

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -7,6 +7,17 @@ namespace GuessingGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Guessing Game!");
+            Console.WriteLine("Pick a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number == 7)
+            {
+                Console.WriteLine("You're a winner!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, wrong number");
+            }
         }
     }
 }
