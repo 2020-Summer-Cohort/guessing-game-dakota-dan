b228002f2cc009fd735d56d9d0bdaa24fd6ce60a
Dakota <Dakotawhittle1203@gmail.com>
2020-05-15 10:34:08 -0400

    added option to exit game

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -9,18 +9,32 @@ namespace GuessingGame
             Console.WriteLine("Welcome to Guessing Game!");
             Console.WriteLine("Pick a number");
             int number = Convert.ToInt32(Console.ReadLine());
-
+            
             if (number == 7)
             {
                 Console.WriteLine("You're a winner!");
             }
             else if (number == 0)
             {
-                Console.WriteLine("Pick a number, and if you guess correctly, you win!");
+                Console.WriteLine("Pick a number, and if you guess correctly, you win! Enter -1 to exit.");
+            }
+            else if (number == -1)
+            {
+                Console.WriteLine("Thanks for playing!");
             }
             else
             {
                 Console.WriteLine("Sorry, wrong number.");
+                Console.WriteLine("Try one more time");
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number == 7)
+                {
+                    Console.WriteLine("You're a winner!");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, wrong number.");
+                }
             }
         }
     }
